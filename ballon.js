575a97dc7d4d5431885b21fd9ff248512cb915d1
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2025-01-08 15:34:55 +0100

    +++

--- ballon.js
+++ ballon.js
@@ -1,8 +1,16 @@
+//ballon
 let size = 100;
 let explod = false;
+//root
+const root = document.documentElement;
+//background
+const currentColor = getComputedStyle(root).getPropertyValue('--main-color');
+let newColor = currentColor;
+//text
+const currentText = getComputedStyle(root).getPropertyValue('--text-color');
+let newText = currentText;
 
 function Bigger(){
-
     size += 10;
 
     if (size >= 500 && size < 509){
@@ -10,12 +18,31 @@ function Bigger(){
     } else if (size >= 510){
         explod = true;
     }
+
     if (explod){
         size = 100;
         document.getElementById("ballon").innerHTML = "🎈";
         explod = false;
     } 
+
     document.getElementById("ballon").style.fontSize = size + "px";
-    
     document.getElementById("size").innerHTML = "Size: " + size + "px";
 }
+
+function DarkAndLightMode(){
+
+    if (newColor == "#111"){
+        document.getElementById("button").innerHTML = "Dark Mode";
+        newColor = "#AAA";
+        newText = "#000";
+    } else if (newColor = "#AAA"){
+        document.getElementById("button").innerHTML = "Light Mode";
+        newColor = "#111";
+        newText = "#AAA";
+    }
+
+    root.style.setProperty('--main-color', newColor);
+    root.style.setProperty('--text-color', newText);
+}
+
+
