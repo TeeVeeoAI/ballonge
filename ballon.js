b31fef89ec99e944360ce0ea9b94a68d66eb923a
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2025-01-09 17:32:59 +0100

    +++

--- ballon.js
+++ ballon.js
@@ -9,9 +9,19 @@ let newColor = currentColor;
 //text
 const currentText = getComputedStyle(root).getPropertyValue('--text-color');
 let newText = currentText;
+//colors
+const defText = "#010101";
+const dark = "#111";
+const light = "#EEE";
+const red = "#FF1A1A";
 
-function Bigger(){
+function Bigger(s, inSize){
+    
     size += 10;
+    if (inSize){
+        size = s;
+    }
+    
 
     if (size >= 500 && size < 509){
         document.getElementById("ballon").innerHTML = "💥";
@@ -31,18 +41,29 @@ function Bigger(){
 
 function DarkAndLightMode(){
 
-    if (newColor == "#111"){
-        document.getElementById("button").innerHTML = "Dark Mode";
-        newColor = "#AAA";
-        newText = "#000";
-    } else if (newColor = "#AAA"){
-        document.getElementById("button").innerHTML = "Light Mode";
-        newColor = "#111";
-        newText = "#AAA";
+    if (newColor == dark){
+        document.getElementById("darkAndLight").innerHTML = "Red Mode";
+        newColor = light;
+        newText = dark;
+    } else if (newColor == light){
+        document.getElementById("darkAndLight").innerHTML = "Dark Mode";
+        newColor = red;
+        newText = defText;
+    } else if (newColor == red){
+        document.getElementById("darkAndLight").innerHTML = "Light Mode";
+        newColor = dark;
+        newText = light;
     }
 
     root.style.setProperty('--main-color', newColor);
     root.style.setProperty('--text-color', newText);
 }
 
-
+function ChangeSize(){
+    let newSize = document.getElementById('inSize').value;
+    newSize = parseInt(newSize);
+    while (newSize % 10 != 0){
+        newSize--;
+    }
+    Bigger(newSize, true);
+}
